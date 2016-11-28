c0761db8ac603ee17b3dfe332eb99f10c95a04db
stergen <stergen-pyx@mail.ru>
2016-11-29 01:37:13 +0200

    add slider and template on product page

--- js/product.js
+++ js/product.js
@@ -37,6 +37,58 @@
   };
 
   window.modules.add('productView', ProductView);
+
+
+  var ProductSlider = function() {
+    this.slider = document.getElementById('slider');
+    this.leftArrow = document.getElementsByClassName('product-images-btn--left');
+    this.rightArrow = document.getElementsByClassName('product-images-btn--right');
+    // this.items = document.getElementsByClassName('product-image');
+
+    this.init();
+  };
+  ProductSlider.prototype.init = function() {
+    var items = document.getElementsByClassName('product-image'),
+        length = items.length;
+    for (var i = 0; i < length; i++) {
+      if (items[i].classList.contains('active') && i > 0) {
+          items[i].classList.remove('active');
+      }
+      items[i].setAttribute('slider-item', i);
+    }    
+
+    var clickHandlerWraper  = document.querySelector(".product-images-btn");
+    clickHandlerWraper.addEventListener('click', this.navigate, false);
+
+  }
+  ProductSlider.prototype.navigate = function() {
+    var items = document.getElementsByClassName('product-image'),
+        current = document.querySelector('.product-image.active'),
+        index = parseInt( current.getAttribute('slider-item') );
+
+    items[index].classList.remove('active');
+    
+    if (event.target.classList.contains('product-images-btn--left')) {
+      if (index === 0) {
+        items[items.length - 1].classList.add('active');
+      } else {
+        items[index-1].classList.add('active');
+      }
+    }
+
+    if (event.target.classList.contains('product-images-btn--right')) {
+      if (index >= items.length - 1) {
+        items[0].classList.add('active');
+      } else {
+        items[index+1].classList.add('active');
+      }
+    }
+
+
+  }
+
+  window.modules.add('productSlider', ProductSlider);
+
 })();
 
 window.app.prototype.init = function() {
--- js/productJSON.js
+++ js/productJSON.js
@@ -5,63 +5,115 @@ window.products =
             "name": "Apple A1466 MacBook Air 13 (Z0TB000JC)",
             "price": 51699,
             "image": '1.jpg',
-            "characteristic": "Экран 13.3 (1440x900) WXGA+ LED, глянцевый / Intel Core i7 (2.2 ГГц) / RAM 8 ГБ / SSD 256 ГБ / Intel HD Graphics 6000 / без ОД / Wi-Fi / Bluetooth / веб-камера / OS X El Capitan / 1.35 кг"
+            "images": [
+                '1_1.jpg',
+                '1_2.jpg',
+                '1_3.jpg',
+                '1_4.jpg',
+                '1_5.jpg'
+            ],
+            "characteristic": {
+                "producer": "Apple",
+                "diagonal": 13.3,
+                "RAM": 8,
+            },
+            "description": "Экран 13.3 (1440x900) WXGA+ LED, глянцевый / Intel Core i7 (2.2 ГГц) / RAM 8 ГБ / SSD 256 ГБ / Intel HD Graphics 6000 / без ОД / Wi-Fi / Bluetooth / веб-камера / OS X El Capitan / 1.35 кг"
         },
         {
             "id": 2,
             "name": "Apple MacBook 12' Rose Gold (MMGL2UA/A)",
             "price": 44199,
             "image": '8.jpg',
-            "characteristic": "Экран 12' Retina IPS (2304x1440) LED, глянцевый / Intel Core M (1.1 - 2.2 ГГц) / RAM 8 ГБ / SSD 256 ГБ / Intel HD Graphics 515 / без ОД / Wi-Fi 802.11ac / Bluetooth 4.0 / веб-камера / OS X El Capitan / 0.92 кг / розовое золото"
+            "characteristic": {
+                "producer": "Apple",
+                "diagonal": 12,
+                "RAM": 8,
+            },
+            "description": "Экран 12' Retina IPS (2304x1440) LED, глянцевый / Intel Core M (1.1 - 2.2 ГГц) / RAM 8 ГБ / SSD 256 ГБ / Intel HD Graphics 515 / без ОД / Wi-Fi 802.11ac / Bluetooth 4.0 / веб-камера / OS X El Capitan / 0.92 кг / розовое золото"
         },  
         {
             "id": 3,
             "name": "Apple MacBook Pro Retina 13' (Z0QP002NP)",
             "price": 77199,
             "image": '9.jpg',
-            "characteristic": "Экран 13.3' IPS (2560x1600) Retina LED, глянцевый / Intel Core i7 (3.1 ГГц) / RAM 16 ГБ / SSD 512 ГБ / Intel Iris Graphics 6100 / без ОД / Wi-Fi / Bluetooth / веб-камера / OS X Yosemite / 1.62 кг"
+            "characteristic": {
+                "producer": "Apple",
+                "diagonal": 13.3,
+                "RAM": 16,
+            },
+            "description": "Экран 13.3' IPS (2560x1600) Retina LED, глянцевый / Intel Core i7 (3.1 ГГц) / RAM 16 ГБ / SSD 512 ГБ / Intel Iris Graphics 6100 / без ОД / Wi-Fi / Bluetooth / веб-камера / OS X Yosemite / 1.62 кг"
         },
         {
             "id": 4,
             "name": "Lenovo ThinkPad E560 (20EVS03M00)",
             "price": 17595,
             "image": '2.jpg',
-            "characteristic": "Экран 15.6 (1366x768) WXGA HD LED, матовый / Intel Core i5-6200U (2.3 - 2.8 ГГц) / RAM 4 ГБ / HDD 500 ГБ / AMD Radeon R7 M370, 2 ГБ / DVD±RW / LAN / Wi-Fi / Bluetooth / веб-камера / DOS / 2.3 кг"
+            "characteristic": {
+                "producer": "Lenovo",
+                "diagonal": 15.6,
+                "RAM": 4,
+            },
+            "description": "Экран 15.6 (1366x768) WXGA HD LED, матовый / Intel Core i5-6200U (2.3 - 2.8 ГГц) / RAM 4 ГБ / HDD 500 ГБ / AMD Radeon R7 M370, 2 ГБ / DVD±RW / LAN / Wi-Fi / Bluetooth / веб-камера / DOS / 2.3 кг"
         },
         {
             "id": 5,
             "name": "Asus VivoBook Flip TP501UA (TP501UA-DN067T) Dark Gray",
             "price": 29700,
             "image": '3.jpg',
-            "characteristic": "Экран 15.6 (1920x1080) FullHD LED, глянцевый, Multitouch / Intel Core i7-6500U (2.5 - 3.1 ГГц) / RAM 8 ГБ / SSD 128 + HDD 1 ТБ / Intel HD Graphics 520 / Без ОД / LAN / Wi-Fi / Bluetooth / веб-камера"
+            "characteristic": {
+                "producer": "Asus",
+                "diagonal": 15.6,
+                "RAM": 8,
+            },
+            "description": "Экран 15.6 (1920x1080) FullHD LED, глянцевый, Multitouch / Intel Core i7-6500U (2.5 - 3.1 ГГц) / RAM 8 ГБ / SSD 128 + HDD 1 ТБ / Intel HD Graphics 520 / Без ОД / LAN / Wi-Fi / Bluetooth / веб-камера"
         },
         {
             "id": 6,
             "name": "Lenovo Yoga 900-13 (80UE007NUA) Orange",
             "price": 43555,
             "image": '4.jpg',
-            "characteristic": "Экран 13.3 IPS (3200x1800) QHD+, Multi-touch, глянцевый / Intel Core i7-6560U (2.2 - 3.2 ГГц) / RAM 8 ГБ / SSD 512 ГБ / Intel Iris Graphics 540 / без ОД / Wi-Fi / Bluetooth / веб-камера / Windows 10 Home / 1.29 кг / оранжевый/"
+            "characteristic": {
+                "producer": "Lenovo",
+                "diagonal": 13.3,
+                "RAM": 8,
+            },
+            "description": "Экран 13.3 IPS (3200x1800) QHD+, Multi-touch, глянцевый / Intel Core i7-6560U (2.2 - 3.2 ГГц) / RAM 8 ГБ / SSD 512 ГБ / Intel Iris Graphics 540 / без ОД / Wi-Fi / Bluetooth / веб-камера / Windows 10 Home / 1.29 кг / оранжевый/"
         },
         {
             "id": 7,
             "name": "HP 255 G4 (N0Y69ES)",
             "price": 5299,
             "image": '5.jpg',
-            "characteristic": "Экран 15.6” (1366x768) HD LED, матовый / AMD Dual-Core E1-6015 (1.4 ГГц) / RAM 2 ГБ / HDD 500 ГБ / AMD Radeon R2 / без ОД / LAN / Wi-Fi / Bluetooth / веб-камера / DOS / 2.15 кг / черный"
+            "characteristic": {
+                "producer": "HP",
+                "diagonal": 15.6,
+                "RAM": 2,
+            },
+            "description": "Экран 15.6” (1366x768) HD LED, матовый / AMD Dual-Core E1-6015 (1.4 ГГц) / RAM 2 ГБ / HDD 500 ГБ / AMD Radeon R2 / без ОД / LAN / Wi-Fi / Bluetooth / веб-камера / DOS / 2.15 кг / черный"
         },
         {
             "id": 8,
             "name": "Asus X555SJ (X555SJ-XO001D/R556SJ-XO061D) Black",
             "price": 7699,
             "image": '6.jpg',
-            "characteristic": "Экран 15.6' (1366x768) HD LED, матовый / Intel Celeron N3050 (1.6 - 2.16 ГГц) / RAM 4 ГБ / HDD 500 ГБ / nVidia GeForce GT 920M, 1 ГБ / DVD Super Multi / LAN / Wi-Fi / Bluetooth / веб-камера / DOS / 2.3 кг / черный"
+            "characteristic": {
+                "producer": "Asus",
+                "diagonal": 15.6,
+                "RAM": 4,
+            },
+            "description": "Экран 15.6' (1366x768) HD LED, матовый / Intel Celeron N3050 (1.6 - 2.16 ГГц) / RAM 4 ГБ / HDD 500 ГБ / nVidia GeForce GT 920M, 1 ГБ / DVD Super Multi / LAN / Wi-Fi / Bluetooth / веб-камера / DOS / 2.3 кг / черный"
         },
         {
             "id": 9,
             "name": "Acer Aspire E5-573G-3894 (NX.MVVEU.013) Black-White",
             "price": 13499,
             "image": '7.jpg',
-            "characteristic": "Экран 15.6'' (1920x1080) Full HD LED, матовый / Intel Core i3-5005U (2.0 ГГц) / RAM 4 ГБ / HDD 1 ТБ / nVidia GeForce 940M, 4 ГБ / DVD Super Multi / LAN / Wi-Fi / Bluetooth / веб-камера / Linux / 2.4 кг / белый с черным"
+            "characteristic": {
+                "producer": "Acer",
+                "diagonal": 15.5,
+                "RAM": 4,
+            },
+            "description": "Экран 15.6'' (1920x1080) Full HD LED, матовый / Intel Core i3-5005U (2.0 ГГц) / RAM 4 ГБ / HDD 1 ТБ / nVidia GeForce 940M, 4 ГБ / DVD Super Multi / LAN / Wi-Fi / Bluetooth / веб-камера / Linux / 2.4 кг / белый с черным"
         }             
     ];
 
